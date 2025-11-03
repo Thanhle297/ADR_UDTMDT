fefa0940239694a7092ccaf81687c9fa7a57b498
Thanhle297 <stu725102086@hnue.edu.vn>
2025-11-03 16:47:50 +0700

    upload test

---
+++ app/src/main/java/com/example/ung_dung_thuong_mai_dien_tu/adapter/TabletAdapter.java
@@ -0,0 +1,4 @@
+package com.example.ung_dung_thuong_mai_dien_tu.adapter;
+
+public class MayTInhBangAdapter {
+}
---
+++ app/src/main/java/com/example/ung_dung_thuong_mai_dien_tu/model/GioHang.java
@@ -0,0 +1,4 @@
+package com.example.ung_dung_thuong_mai_dien_tu.model;
+
+public class GioHang {
+}
